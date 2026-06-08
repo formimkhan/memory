a81a3d5a988ea8f783d789868a080791ec6c6803
mahbuburrahman05 <mbub880@gmail.com>
2026-06-08 10:35:35 -0700

    done

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ item.classList.add('active');
 
 });
 
-
 for(let i=0;i<40;i++){
 
 const heart=document.createElement("div");
@@ -43,3 +42,35 @@ direction:"alternate"
 document.body.appendChild(heart);
 
 }
+
+/* AUTO POSITION ALL PHOTOS */
+
+const allCards =
+document.querySelectorAll('.floating-card');
+
+const positions = [
+    '5%',
+    '38%',
+    '70%'
+];
+
+allCards.forEach((card,index)=>{
+
+    card.style.top =
+    (index * 300 + 100) + 'px';
+
+    card.style.left =
+    positions[index % 3];
+
+    card.style.transform =
+    `rotate(${Math.random()*14 - 7}deg)`;
+
+});
+
+/* AUTO GALLERY HEIGHT */
+
+const gallery =
+document.querySelector('.floating-gallery');
+
+gallery.style.minHeight =
+(allCards.length * 320 + 500) + 'px';
